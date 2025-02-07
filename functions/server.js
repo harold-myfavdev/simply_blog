a47be00d1e34d7d3ebb57dda81da1483a7a82df9
harold-webdev <harold.webdev@myfav-dev>
2025-02-07 17:23:14 +0800

    fix D1 bindings

--- functions/server.js
+++ functions/server.js
@@ -1,7 +1,7 @@
 export async function onRequest(context) {
     const t0 = performance.now(); 
     // Create a prepared statement with our query
-    const ps = context.env.DB.prepare("SELECT * from Customers");
+    const ps = context.env.BLOGS_DB.prepare("SELECT * from Customers");
     const data = await ps.all();
     const t1 = performance.now();     
     console.log(`Call to doSomething took ${t1 - t0} milliseconds.`); 
