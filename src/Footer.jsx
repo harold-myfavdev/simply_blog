eceacd18075e78b8a6e1caa5582a071311aff0a8
harold-webdev <harold.webdev@myfav-dev>
2025-02-07 20:48:23 +0800

    add footer

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Copyright 2025 Simply Blog by MyFAV</p>
+        </footer>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client';
 import '/src/index.css';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 createRoot(document.getElementById('root')).render(
   <>
     <Header />
     <Main />
+    <Footer />
   </>
 )
