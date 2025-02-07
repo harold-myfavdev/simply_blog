5293df0699fced24cdf12d53e9aef15ef2116124
harold-webdev <harold.webdev@myfav-dev>
2025-02-07 15:32:23 +0800

    scrimba first project

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import '/src/index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <>
+    <header>
+      <img src="/public/react-logo.png" alt="react-logo" width="40px;" height="40px"/>
+      <h1>React Facts</h1>
+    </header>
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walkie</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  </>
+)
