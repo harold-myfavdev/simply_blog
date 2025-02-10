c220d9bd08a412cbf827b25af4ef849727ace9c3
harold-webdev <harold.webdev@myfav-dev>
2025-02-11 00:20:09 +0800

    use data api to dynamically generate post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,17 @@
-export default function Post(props) {
-    /* Get post start and end dates */
-    const postStartDate = props.postStartDate;
-    const postEndDate = props.postEndDate;
-    const [dayStart, monthNameStart, yearStart] = postStartDate.replace(",", "").split(" ");
-    const [dayEnd, monthNameEnd, yearEnd] = postEndDate.replace(",", "").split(" ");
-    function getMonthNumber(monthName) {
-        const months = {
-            Jan: "01", Feb: "02", Mar: "03", Apr: "04", May: "05", Jun: "06",
-            Jul: "07", Aug: "08", Sep: "09", Oct: "10", Nov: "11", Dec: "12"
-        };
-        return months[monthName] || "Invalid month";
-    }
-    const monthNumStart = getMonthNumber(monthNameStart);    
-    const monthNumEnd = getMonthNumber(monthNameEnd);    
-    const dateTimeStart = `${yearStart}-${monthNumStart}-${dayStart}`;    
-    const dateTimeEnd = `${yearEnd}-${monthNumEnd}-${dayEnd}`;    
-    /* End get post start and end dates */
+export default function Post(props) {  
 
     return (
         <>
             <figure className="postContainer">
-                <img src={props.postImg} alt="Mount Fuji" className="postImg"/>
+                <img src={props.postImg} alt={props.postImgAlt} className="postImg"/>
                 <div className="postDetails">
                     <p className="postLocation">
                         <img src={props.marker} alt="Location Marker" />
-                        JAPAN
+                        {props.country}
                         <a href={props.map} className="googleMap">View on Google Maps</a>
                     </p>
                     <h2 className="postTitle">{props.postTitle}</h2>
-                    <p className="postDate"><time dateTime={dateTimeStart}></time>{props.postStartDate} - <time dateTime={dateTimeEnd}>{props.postEndDate}</time></p>
+                    <p className="postDate">{props.postDateRange}</p>
                     <p className="postDesc">{props.postDesc}</p>
                 </div>
             </figure>
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,54 +1,28 @@
 import MountFuji from '/src/assets/Mount Fuji.png';
 import Marker from '/src/assets/Marker.svg';
 import Post from './Post';
+import postsData from '../api/data';
 
 export default function Posts() {
-    const postsData = [
-        {postImg : MountFuji,
-         marker : Marker,
-         map : "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
-         postTitle : "Mount Fuji",
-         postStartDAte : "12 Jan, 2021",
-         postEndDAte : "24 Jan, 2023",
-         postDesc : "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        },
-
-        {postImg : MountFuji,
-            marker : Marker,
-            map : "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
-            postTitle : "Mount Fuji",
-            postStartDAte : "12 Jan, 2021",
-            postEndDAte : "24 Jan, 2023",
-            postDesc : "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        },
-
-        {postImg : MountFuji,
-            marker : Marker,
-            map : "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
-            postTitle : "Mount Fuji",
-            postStartDAte : "12 Jan, 2021",
-            postEndDAte : "24 Jan, 2023",
-            postDesc : "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        },
-
-    ]
+    console.log(postsData);
+    const posts = postsData.map((post)=>(
+        <Post 
+            key={post.id}
+            marker={Marker} 
+            postImg={post.img.src} 
+            postImgAlt={post.img.alt}
+            postTitle={post.title}
+            postCountry={post.country}
+            map={post.googleMapsLink}
+            postDateRange={post.dates}            
+            postDesc={post.text}
+        />
+        )
+    )
 
     return (
         <>
-            {postsData.map((post, i)=>(
-                <Post 
-                    key={i}
-                    postImg={postsData[i].postImg} 
-                    marker={postsData[i].marker} 
-                    map={postsData[i].map}
-                    postTitle={postsData[i].postTitle}
-                    postStartDate={postsData[i].postStartDAte}
-                    postEndDate={postsData[i].postEndDAte}
-                    postDesc={postsData[i].postDesc}
-                />
-                )
-            )}
-            
+            {posts}            
         </>
     )
 }
