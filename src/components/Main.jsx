0b94325c80961db83e4bacb8e344f44be90e48bc
harold-webdev <harold.webdev@myfav-dev>
2025-02-13 11:26:33 +0800

    add temp post data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
 import Posts from './Posts';
+import './main.css'
 
 export default function Main() {
     return (
         <main>
-            <Posts />
+            <section className='blog-posts' id='recent-blog-posts'>   
+                <h2>Recent blog posts</h2>            
+                <div className='blog-posts-wrapper'>
+                    <Posts />
+                </div>
+            </section>
         </main>
     )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,7 +15,6 @@ export default function Post(props) {
                     <p className="postDesc">{props.postDesc}</p>
                 </div>
             </figure>
-            <hr />
         </>
     )
 }
