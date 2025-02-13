81f5523552fccd9e5c5d6f8a931fcce4b2c2a7f9
harold-webdev <harold.webdev@myfav-dev>
2025-02-13 17:17:32 +0800

    add Sample jsx css

---
+++ src/components/Sample.jsx
@@ -0,0 +1,13 @@
+import './sample.css'
+
+export default function Sample() {
+    return (
+        <div className="container">
+            <div className="h-100">100</div>
+            <div className="h-50">50</div>
+            <div className="h-50">50</div>
+            <div className="h-100">100</div>
+            <div className="h-50">50</div>
+        </div>
+    )
+}
