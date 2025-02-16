d8dbd1156efbd6f8df028bf2d2cee29a5f3de41f
harold-webdev <harold.webdev@myfav-dev>
2025-02-17 02:17:34 +0800

    adjust css recent-blog-post section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,16 @@ export default function Main() {
     return (
         <main>
             <section className='blog-posts' id='recent-blog-posts'>   
-                <h2>Recent blog posts</h2>            
-                <div className='blog-posts-wrapper'>
+                <h2 className='blog-posts-header'>Recent blog posts</h2>            
+                <div className='blog-posts-wrapper blog-preview-1'>
                     <Posts limit={1} />
                 </div>
-                <div className='blog-posts-wrapper'>
+                <div className='blog-posts-wrapper blog-preview-2'>
                     <Posts limit={2} />
                 </div>
+                {/* <div className='blog-posts-wrapper blog-preview-3'>
+                    <Posts limit={1} />
+                </div> */}
             </section>
         </main>
     )
