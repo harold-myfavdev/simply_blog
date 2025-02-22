4f44a12589cd647e42de7b38db8e91651f9e7784
harold-webdev <harold.webdev@myfav-dev>
2025-02-22 21:22:31 +0800

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import './footer.css';
+
+export default function Footer() {
+    const [copyrightYear] = useState(new Date().getFullYear())
+        
+    
+    return (
+        <footer className='footer'>
+            <p className="copyright-text">  &copy; <span className="copyright-year">{copyrightYear}</span></p>
+            <ul className='external-links'>
+                <li className="external-link-item"><a href="">Twitter</a></li>
+                <li className="external-link-item"><a href="">LinkedIn</a></li>
+                <li className="external-link-item"><a href="">Email</a></li>
+                <li className="external-link-item"><a href="">RSS feed</a></li>
+                <li className="external-link-item"><a href="">Add to Feedly</a></li>
+            </ul>
+        </footer>
+    )
+}
