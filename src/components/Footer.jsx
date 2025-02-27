ae1477230b394c8c6ff45dfcfe6785b2d126463a
harold-webdev <harold.webdev@myfav-dev>
2025-02-28 02:31:53 +0800

    add fetch data button to test restricted fetch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import './footer.css';
 
 export default function Footer() {
     const [copyrightYear] = useState(new Date().getFullYear())
-        
+    async function fetchData() {
+        const response = await fetch('https://myfav.dev/fetch')
+        const data = await response.json()
+        console.log(data)
+    }
     
     return (
         <footer className='footer'>
@@ -15,6 +19,7 @@ export default function Footer() {
                 <li className="external-link-item"><a href="">RSS feed</a></li>
                 <li className="external-link-item"><a href="">Add to Feedly</a></li>
             </ul>
+            <button onClick={fetchData}>Fetch Data</button>
         </footer>
     )
 }
